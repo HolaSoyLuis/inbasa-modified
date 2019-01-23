4fb6baddb4bf730876e93083ec20c3aa304ee818
Gerber Maldonado <g.maldonadof25@gmail.com>
2019-01-22 21:37:44 -0600

    Grado
    -Se Modifico el modulo de grado

--- resources/views/admin/grados_secciones/grados/grados.blade.php
+++ resources/views/admin/grados_secciones/grados/grados.blade.php
@@ -28,48 +28,6 @@
 
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-<<<<<<< HEAD
-		<div class="table-responsive">
-			<table class="table table-striped table-bordered table-condensed table-hover">
-				<thead>
-					<th>No</th>
-					<th>Jornada</th>
-					<th>Grado</th>
-					<th>Creado</th>
-					<th>Actualizado</th>
-					<th>Opciones</th>
-				</thead>
-				@foreach($grados as $grado)		
-				<tr>
-					<td>{{$grado->id}}</td>
-					<td>{{$grado->jornada}}</td>
-					<td>{{$grado->grado}}</td>
-					<td>{{$grado->created_at}}</td>
-					<td>{{$grado->updated_at}}</td>
-					<td>
-						<a href="#">
-							<button class="btn btn-info btn-sm" title="Ver">
-								<i class="material-icons">visibility</i>
-								Ver
-							</button>
-						</a>
-						<a href="#">
-							<button class="btn btn-primary btn-sm" title="Editar">
-								<i class="material-icons">edit</i>
-								Editar
-							</button>
-						</a>
-						<a href="" data-target="#" data-toggle="modal">
-							<button class="btn btn-danger btn-sm" title="Eliminar">
-								<i class="material-icons">delete</i>
-								Eliminar
-							</button>
-						</a>
-					</td>
-				</tr>
-				@endforeach
-			</table>
-=======
 		<div class="card">
 			<div class="card-body">
 				<div class="table-responsive">
@@ -80,7 +38,7 @@
 							<th>Jornada</th>
 							<th>Creado</th>
 							<th>Actualizado</th>
-							<!-- <th>Opciones</th> -->
+							<th>Opciones</th>
 						</thead>		
 						@foreach($grados as $grado)		
 						<tr>
@@ -89,7 +47,7 @@
 							<td>Vespertina</td>
 							<td>{{$grado->created_at}}</td>
 							<td>{{$grado->updated_at}}</td>					
-							<!-- <td>
+							<td>
 								<a href="#">
 									<button class="btn btn-info btn-sm" title="Ver">
 										<i class="material-icons">visibility</i>
@@ -108,13 +66,12 @@
 										Eliminar
 									</button>
 								</a>
-							</td> -->
+							</td>
 						</tr>	
 						@endforeach						
 					</table>
 				</div>
 			</div>		
->>>>>>> debdea7820a88d772a31c02b818360db63cc4eff
 		</div>
 	</div>
 </div>
